be2c0ae8f03e23b41412b2db72ae263d1d83b74b
glory <844264382@qq.com>
2020-01-05 22:36:13 +0800

    add vue独立组件

--- ui/src/main.js
+++ ui/src/main.js
@@ -5,7 +5,36 @@ import $ from 'jquery'
 import './css/index.css'
 import './css/index.scss'
 
-$(function() {
-    $('li:odd').css('backgroundColor','pink');
-    $('li:even').css('backgroundColor','yellow');
-})
+import Vue from 'Vue'
+//导入App.vue组件，默认webpack无法打包.vue文件，需要安装相关的loader :
+//cnpm i vue-loader vue-template-compiler -D
+//在配置文件中，新增loader的配置项
+import home from './home.vue'
+
+// import VUeRouter from 'vue-router'
+//手动安装router
+// Vue.use(VUeRouter)
+
+//导入Mint-UI
+import MintUI from 'mint-ui'
+//这里可以省略 node_modules这层目录 
+import 'mint-ui/lib/style.css'
+//将MintUI安装到Vue中
+Vue.use(MintUI)
+
+// $(function() {
+//     $('li:odd').css('backgroundColor','whrite');
+//     $('li:even').css('backgroundColor','blue');
+// })
+
+var vm = new Vue({
+    el : "#app",
+    // data : {
+    //     msg : '123'
+    // },
+    // render : function(createElements) {
+    //     return createElements(home)
+    // }
+    render : c => c(home) //相当于上面的写法
+
+}) 
--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -5,6 +5,9 @@ const path = require('path')
 //如果要配置插件，则要在导出的对象中挂载一个plugins节点
 const htmlWebpackPlugin = require('html-webpack-plugin')
 
+//Vue Loader v15现在需要一个附带的webpack插件才能正常工作
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
 module.exports= {
     entry: path.join(__dirname, './src/main.js'), //入口，表示要使用webpack打包的对应文件
     output: {   //输出文件的相关配置
@@ -15,7 +18,8 @@ module.exports= {
                 new htmlWebpackPlugin({
                     template : path.join(__dirname, './src/index.html'), //指定模板文件路径
                     filename : 'index.html' //设置生成的内存页面名称
-                })
+                }),
+                new VueLoaderPlugin(),
     ],
     module : { //配置所有第三方loader模块的匹配规则
         rules : [
@@ -24,6 +28,8 @@ module.exports= {
             {test : /\.scss$/, use : ['style-loader', 'css-loader', 'sass-loader']} ,//处理sass文件类型的loader
             //处理图片路径
             {test : /\.(jpg|png|gif|bmp|jpeg)$/, use : 'url-loader'} ,
+            //处理.vue文件的loader
+            {test : /\.vue$/, use : 'vue-loader'},
         ]
     }
 }
